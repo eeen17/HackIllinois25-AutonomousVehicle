38041e413ee1fefff9b18e3259c80b801c9f23ee
eeen17 <ethanytchang@gmail.com>
2025-03-02 01:37:13 -0600

    motor cange

--- Code/Client/Main.py
+++ Code/Client/Main.py
@@ -66,8 +66,8 @@ class mywindow(QMainWindow,Ui_Client):
         self.color_custom_flag = False
         self.color_custom_flag2 = False
         self.color_select_button = 0
+        # brg
         self.color_red =   [0, 118, 31, 6, 255, 255]
-        self.color_green = [50, 143, 129, 60, 255, 255]
         self.color_green = [50, 143, 129, 60, 255, 255]
         self.color_blue =  [100, 94, 142, 120, 255, 255]
         self.color_purple = [140, 100, 100, 160, 255, 255]
@@ -780,23 +780,23 @@ class mywindow(QMainWindow,Ui_Client):
                     cv2.circle(video, (int(X), int(Y)), 3, (255, 0, 0),5)
                     cv2.circle(video, (int(X), int(Y)), int(radius), (0, 255, 0), 2)
                     if d < 5:
-                        self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-300) + '#' + str(-300) + self.endChar)
+                        self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-600) + '#' + str(-600) + self.endChar)
                     elif d > 40:
-                        self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(300) + '#' + str(300) + self.endChar)
+                        self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(600) + '#' + str(600) + self.endChar)
                     else:
                         if x < 85:
-                            self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-675) + '#' + str(675) + self.endChar)
+                            self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-1350) + '#' + str(1350) + self.endChar)
                         elif x > 315:
-                            self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(675) + '#' + str(-675) + self.endChar)
+                            self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(1350) + '#' + str(-1350) + self.endChar)
                         else:
                             self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(0) + '#' + str(0) + self.endChar)
                 else:
                     # rotate until see ball
-                    self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-675) + '#' + str(675) + self.endChar)
+                    self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-1350) + '#' + str(1350) + self.endChar)
                     # self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(0) + '#' + str(0) + self.endChar)
-                    # self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-675) + '#' + str(675) + self.endChar)
+                    # self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-1350) + '#' + str(1350) + self.endChar)
             else:
-                self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-675) + '#' + str(675) + self.endChar)
+                self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(-1350) + '#' + str(1350) + self.endChar)
                 # self.TCP.sendData(cmd.CMD_MOTOR + '#' + str(0) + '#' + str(0) + self.endChar)
         except:
             pass
